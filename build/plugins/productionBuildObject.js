5015757d7edff7ff2954ac8cd9aa0f11343e80d1
郭正中 <guozhengzhong@tal.com>
2023-04-10 22:35:21 +0800

    feat: 可以打包

--- build/plugins/productionBuildObject.js
+++ build/plugins/productionBuildObject.js
@@ -1,18 +1,18 @@
 import path from 'path';
 import fs from 'fs';
-class BuildObject {
-   //编译主进程代码
-   buildMain() {
+export default class BuildObject {
+  //编译主进程代码
+  buildMain() {
     require("esbuild").buildSync({
-        entryPoints: ["./electron/main/index.js"],
-        bundle: true,
-        minify: true,
-        platform: "node",
-        outfile: "./dist/main.js",
-        external: ["electron"],
+      entryPoints: ["./electron/main/index.js"],
+      bundle: true,
+      minify: true,
+      platform: "node",
+      outfile: "./dist/main.js",
+      external: ["electron"],
     })
   }
-      //为生产环境准备package.json
+  //为生产环境准备package.json
   preparePackageJson() {
     let pkgJsonPath = path.join(process.cwd(), "package.json");
     let localPkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf-8"));
@@ -24,5 +24,54 @@ class BuildObject {
     let tarJsonPath = path.join(process.cwd(), "dist", "package.json");
     fs.writeFileSync(tarJsonPath, JSON.stringify(localPkgJson));
     fs.mkdirSync(path.join(process.cwd(), "dist/node_modules"));
+  }
+
+  /**
+* @type {import('electron-builder').Configuration}
+* @see https://www.electron.build/configuration/configuration
+*/
+  buildInstaller() {
+    const options = {
+      config: {
+        // "store” | “normal” | "maximum". - For testing builds, use 'store' to reduce build time significantly.
+        compression: "normal",
+        removePackageScripts: true,
+        nodeGypRebuild: false,
+
+        directories: {
+          output: path.join(process.cwd(), "release"),
+          app: path.join(process.cwd(), "dist"),
+        },
+        files: [
+          "**"
+        ],
+        extends: null,
+        productName: "vue3-electron",
+        appId: 'com.vue3-electron',
+        asar: true,
+        nsis: {
+          oneClick: true,
+          perMachine: true,
+          allowToChangeInstallationDirectory: true,
+          createDesktopShortcut: true,
+          createStartMenuShortcut: true,
+          shortcutName: "vue3Desktop",
+          deleteAppDataOnUninstall: true,
+        },
+
+        mac: {
+          target: 'dmg',
+          hardenedRuntime: true,
+          gatekeeperAssess: true,
+          extendInfo: {
+            NSAppleEventsUsageDescription: 'Let me use Apple Events.',
+            NSCameraUsageDescription: 'Let me use the camera.',
+            NSScreenCaptureDescription: 'Let me take screenshots.',
+          }
+        }
+      },
+      project: process.cwd(),
+    };
+    return require("electron-builder").build(options);
   }
 }
--- build/plugins/viteForElectronProPlugin.js
+++ build/plugins/viteForElectronProPlugin.js
@@ -1,8 +1,12 @@
+import BuildObject from './productionBuildObject';
 export const viteProPlugin = () => {
   return {
     name: "build-plugin",
     closeBundle() {
       const build = new BuildObject();
+      build.buildMain();
+      build.preparePackageJson();
+      build.buildInstaller();
     }
   };
 }
